670535374da13c95c5ba0fdbf1db981135c08a82
renepip <renepipersburgh@aol.com>
2021-12-19 16:33:37 -0500

    First Push

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Calculator {
+    //creating a class named calculator
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Calculator");
+        Scanner input = new Scanner(System.in);
+
+
+
+
+    }
+
+
+
+}
